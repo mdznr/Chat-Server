92e3cc3e8a9a798c7385ee81d3f9b1224e1e4018
Matt Zanchelli <matt@mdznr.com>
2014-03-24 10:39:55 -0400

    Added C++ Xcode Project.

---
+++ Chat Server/Chat Server/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  Chat Server
+//
+//  Created by Matt Zanchelli on 3/24/14.
+//  Copyright (c) 2014 Matt Zanchelli. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+int main(int argc, const char * argv[])
+{
+	cout << "Hello, World!" << endl;
+    return 0;
+}
+
