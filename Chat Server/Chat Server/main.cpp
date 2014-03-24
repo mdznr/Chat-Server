fd3f0a3fe4a955b591db84821c9d81f6e74b5428
Matt Zanchelli <matt@mdznr.com>
2014-03-24 11:33:34 -0400

    Finding verbose mode option (-v) in command line arguments. Port numbers are messed up because of this (for now).

--- Chat Server/Chat Server/main.cpp
+++ Chat Server/Chat Server/main.cpp
@@ -8,10 +8,14 @@
 
 #include <iostream>
 #include <vector>
+#include <map>
+
+// Your server must support at least 32 concurrent clients.
+#define MAX_CONCURRENT_CLIENTS 32
 
 using namespace std;
 
-/// Pull the ports into a vector from the command-line arguments.
+/// Pull the ports into a vector from command-line arguments.
 /// @param argc The number of arguments (items in @c argv).
 /// @param argv The argument vector.
 /// @return A vector of port numbers.
@@ -23,7 +27,7 @@ vector<int> getPorts(int argc, const char * argv[])
 	vector<int> ports;
 	
 	// Collect all port numbers in relevant arguments.
-	for ( unsigned int i=1; i<argc; ++i ) {
+	for ( int i=1; i<argc; ++i ) {
 		const char *port = argv[i];
 		unsigned int portNumber = atoi(port);
 		ports.push_back(portNumber);
@@ -33,11 +37,52 @@ vector<int> getPorts(int argc, const char * argv[])
 	return ports;
 }
 
-int main(int argc, const char * argv[])
+/// Validate and parse command-line arguments.
+/// @param argc The number of arguments (items in @c argv).
+/// @param argv The argument vector.
+/// @return Whether or not this function was successful.
+bool getArguments(int argc, const char * argv[], map<string, bool> *options)
 {
 	// Check if there's at least one *real* argument (port no.).
 	if ( argc < 2 ) {
 		cerr << "Must specify at least one port number.\nUsage: chat_server <port> [<port> ... <port>]";
+		return false;
+	}
+	
+	// Get the argument in a C++ vector.
+	vector<string> arguments;
+	for ( int i=1; i<argc; ++i ) {
+		string argument = string(argv[i]);
+		arguments.push_back(argument);
+	}
+	
+	// Find any specified options.
+	for ( int i=0; i<arguments.size(); ++i ) {
+		string argument = arguments[i]; // Current argument being checked.
+		string prefix("-"); // Prefix that appears before options.
+		if ( !argument.compare(0, prefix.size(), prefix) ) {
+			string option = argument.substr(prefix.size(), argument.size()-prefix.size());
+			// TODO: Remove option from vector.
+			options->insert(pair<string, bool>(option, true));
+		}
+	}
+	
+	return true;
+}
+
+int main(int argc, const char * argv[])
+{
+	// The options for running the program.
+	// -v: Verbose Mode (Default: OFF).
+	map<string, bool> options;
+	
+	bool VERBOSE_MODE = false;
+	if ( options["v"] ) {
+		VERBOSE_MODE = true;
+	}
+	
+	// Validate and parse the arguments.
+	if ( !getArguments(argc, argv, &options) ) {
 		return EXIT_FAILURE;
 	}
 	
@@ -52,7 +97,7 @@ int main(int argc, const char * argv[])
 	} else {
 		// More than one port (plural).
 		cout << "Starting Chat Server on ports " << ports[0];     // First port.
-		for ( unsigned int i=1; i<ports.size()-1; ++i ) {
+		for ( int i=1; i<ports.size()-1; ++i ) {
 			cout << ", " << ports[i];                             // All middle ports.
 		}
 		cout << ", and " << ports[ports.size()-1] << "." << endl; // Last port.
