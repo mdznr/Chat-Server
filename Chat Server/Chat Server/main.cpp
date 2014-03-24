2a0298acb0c9ab8306583b13b9255c6812e9ef67
Matt Zanchelli <matt@mdznr.com>
2014-03-24 10:59:56 -0400

    Getting port numbers from the command line arguments.

--- Chat Server/Chat Server/main.cpp
+++ Chat Server/Chat Server/main.cpp
@@ -7,11 +7,53 @@
 //
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
+
+/// Pull the ports into a vector from the command-line arguments.
+/// @param argc The number of arguments (items in @c argv).
+/// @param argv The argument vector.
+/// @return A vector of port numbers.
+vector<int> getPorts(int argc, const char * argv[])
+{
+	/*
+	 USAGE:
+	 chat_server <port> [<port> ... <port>]
+	 */
+	vector<int> ports;
+	for ( unsigned int i=1; i<argc; ++i ) {
+		const char *port = argv[i];
+		unsigned int portNumber = atoi(port);
+		ports.push_back(portNumber);
+	}
+	return ports;
+}
+
 int main(int argc, const char * argv[])
 {
-	cout << "Hello, World!" << endl;
-    return 0;
+	// Check if there's at least one *real* argument (port no.).
+	if ( argc < 2 ) {
+		cerr << "Must specify at least one port number.\nUsage: chat_server <port> [<port> ... <port>]";
+		return EXIT_FAILURE;
+	}
+	
+	// Get all the ports to have the chat server run on.
+	vector<int> ports = getPorts(argc, argv);
+	
+#ifdef DEBUG
+	// Print "Starting Chat Server on port(s)...".
+	if ( ports.size() == 1 ) {
+		cout << "Starting Chat Server on port " << ports[0] << "." << endl;
+	} else {
+		cout << "Starting Chat Server on ports " << ports[0]; // First port.
+		for ( unsigned int i=1; i<ports.size()-1; ++i ) {
+			cout << ", " << ports[i]; // All middle ports.
+		}
+		cout << ", and " << ports[ports.size()-1] << "." << endl; // Last port.
+	}
+#endif
+	
+    return EXIT_SUCCESS;
 }
 
