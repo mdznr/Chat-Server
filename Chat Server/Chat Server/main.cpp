203d3f2c8b003c5076e36bad62d86a1ce8ccac75
Matt Zanchelli <matt@mdznr.com>
2014-03-26 21:06:52 -0400

    Setting up server.

--- Chat Server/Chat Server/main.cpp
+++ Chat Server/Chat Server/main.cpp
@@ -8,8 +8,28 @@
 
 #include "CommandLineArguments.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <limits.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#include <pthread.h>
+
+#include <sys/errno.h>
+#include <sys/select.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+
+#define BUFFER_SIZE 2048
+
 // Your server must support at least 32 concurrent clients.
-#define MAX_CONCURRENT_CLIENTS 32
+#define MAX_CLIENTS 32
 
 using namespace std;
 
@@ -73,5 +93,61 @@ int run(string name, map<string, bool> options, vector<string> misc)
 	}
 #endif
 	
+	// Create the listener socket as TCP socket. (use SOCK_DGRAM for UDP)
+	int sock = socket(PF_INET, SOCK_STREAM, 0);
+	if ( sock < 0 ) {
+		perror("socket()");
+		exit(1);
+	}
+	
+#warning Handle multiple ports.
+	unsigned short port = ports[0];
+	
+	// Create the server.
+	struct sockaddr_in server;
+	server.sin_family = PF_INET;
+	server.sin_addr.s_addr = INADDR_ANY;
+	server.sin_port = htons(port);
+	
+	// Bind.
+	bind(sock, (struct sockaddr *) &server, sizeof(server));
+	
+	// Create the client.
+	struct sockaddr_in client;
+	socklen_t fromlen = sizeof(client);
+	listen(sock, MAX_CLIENTS); // 5 is the number of backlogged waiting clients.
+	printf("Listener socket created and bound to port %d\n", port);
+	
+	// Keep taking requests from client.
+	while (true) {
+		// Accept client connection.
+		int fd = accept(sock, (struct sockaddr *) &client, &fromlen);
+		
+#ifdef DEBUG
+		printf("Accepted client connection on fd: %d\n", fd);
+#endif
+		
+		// Create a buffer to read the message into.
+		char buffer[BUFFER_SIZE];
+		
+		// Receive the message.
+		ssize_t n = recv(fd, buffer, BUFFER_SIZE - 1, 0);
+		// Check recv() return value.
+		if ( n <= 0 ) {
+			// Errored.
+			perror("recv()");
+			continue;
+		} else {
+			// Stream received message.
+			buffer[n] = '\0';
+#ifdef DEBUG
+			//printf("Received message from fd %d: %s\n", fd, buffer);
+#endif
+		}
+		
+		// Handle message.
+	}
+	
+	// We'll never get here.
     return EXIT_SUCCESS;
 }
